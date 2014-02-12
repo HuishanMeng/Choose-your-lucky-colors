a2ffaf777c4421e67996b2aad0dcf50a558f6d0b
shan1023 <Alison_M@Huishans-MacBook-Pro.local>
2014-02-12 01:22:57 -0500

    choose your lucky color_New

--- main.lua
+++ main.lua
@@ -5,25 +5,69 @@ local myText={
 [3]="Green",
 [4]="Blue",
 [5]="Purple",
-[6]="Gold",
-[7]="White"                                                     
+[6]="Red",
+[7]="White"                                                   
 }
-local background = display.newImage("rainbow.jpg",100,70)
+local myColor={
+  [1]={1/255,1/255,1/255}, 
+  [2]={255/255,115/255,35/255},
+  [3]={35/255,255/255,100/255},
+  [4]={17/255,153/255,255/255},
+  [5]={166/255,51/255,255/255},
+  [6]={255/255,37/255,7/255},
+  [7]={255/255,255/255,255/255},                                                     
+  }
+
+
+
+local background = display.newImage("rainbow.jpg",150,200)
 local allText=myText[1].." "..myText[2].." "..myText[3].." "..myText[4].." "..
 myText[5].." "..myText[6].." "..myText[7]
+
 local displayWord = display.newText(allText,155,80,"Comic Sans MS",15)
-displayWord:setTextColor(1)
+--displayWord:setFillColor(1)
 local inText=display.newText("Shake for your lucky color",150,150,"Comic Sans MS",15)
-local textIndex = math.random(1,7)
---myColor.textIndex=myText.textIndex
-local function onShake(event)
+--inText:setFillColor(120/225,10/120,120/255)
+local randomWord
+function onShake(event)
+	
 	if event.isShake then
-	local randomWord = display.newText(myText[textIndex],160,300,"Comic Sans MS",20)	
-   	--local myCircle = display.newCircle( 220, 305, 15 )  
+    local textIndex = math.random(1,7)
+    local colorIndex= textIndex
+	 randomWord = display.newText(myText[textIndex],160,300,"Comic Sans MS",20)	
+   	local myCircle = display.newCircle( 220, 305, 15 ) 
+   	myCircle:setFillColor(unpack(myColor[colorIndex]))
+   	print(colorIndex)
+   
+	end
 end
-end
+
 --creat a runtime listener to check whether the user shakes the phone  
 --http://thatssopanda.com/corona-sdk-tutorials/using-the-shake-event-with-corona-sdk/
-Runtime:addEventListener("accelerometer", onShake)
+if randomWord==nil then
+	Runtime:addEventListener("accelerometer", onShake)
+	--randomWord=display.newText("")
 
+else
+randomWord:removeSelf()
+randomWord=nil
+end
+function Removeyixia()
+	randomWord:removeSelf()
+	randomWord=nil
+end
+
+
+
+-- if randomWord ~= nil then
+-- randomWord:removeSelf()
+-- randomWord=nil
+-- --Runtime:addEventListener("accelerometer", onShake)
+-- else 
+-- Runtime:addEventListener("accelerometer", onShake)
+
+-- end
+
+--I am not sure what this means.
+--randomWord:removeSelf()
 
